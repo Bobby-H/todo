309fbba342be8607992c669d27a67ed944865ba3
Bobby-H <imbobbyhutter@gmail.com>
2022-02-13 05:44:42 -0500

    attempting to get /todos to load from port 3000

--- todo/index.js
+++ todo/index.js
@@ -4,10 +4,10 @@ const app = express
 
 const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb://localhost:27017/todos', function(err, client) {
+MongoClient.connect('mongodb://localhost:3000/todos', function(err, client) {
     if (err) throw err
 
-    var db = client.db('todos')
+    db = client.db('todos')
 
     db.collection('mammals').find().toArray(function(err, result) {
         if (err) throw err
@@ -19,6 +19,12 @@ MongoClient.connect('mongodb://localhost:27017/todos', function(err, client) {
 app.get('/', (req, res) => {
     res.json('did this work');
 });
+
+app.get('/todos', async(req, res) => {
+    const todos = await db.collection('todos').find().toArray();
+    res.json(todos);
+});
+
 
 app.listen(3000, () => {
     console.log('im working!')
